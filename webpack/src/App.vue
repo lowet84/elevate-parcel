1cc545070e14eb2e1ac5fe8684da9abec7171ec3
Fredrik Löwenhamn <lowet84@gmail.com>
2019-09-08 11:19:09 +0200

    Wip

---
+++ webpack/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="presentation container">
+    <div>{{ msg }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: '!hejsdfsdf'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  height: 100vh;
+}
+</style>
---
+++ webpack/src/index.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App from './App'
+
+import '../../parcel/src/styles.css'
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
