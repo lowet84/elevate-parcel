0daa874c90702f5464ba12e944cd9f5c20d6b13c
Fredrik Löwenhamn <lowet84@gmail.com>
2019-11-28 11:25:24 +0100

    Added dummy to proxy example

--- extra/proxy.js
+++ extra/proxy.js
@@ -3,7 +3,19 @@ const proxyMiddleware = require("http-proxy-middleware");
 
 var app = express();
 
-app.use("/api", proxyMiddleware("http://localhost:3000"));
+app.use("/api", proxyMiddleware("http://localhost:3100"));
 app.use("/", proxyMiddleware("http://localhost:1234"));
 
 app.listen(3800);
+
+
+
+// dummy
+
+var dummyApp = express()
+
+dummyApp.use('/api', (req,res)=>{
+    res.send({value: "something"})
+})
+
+dummyApp.listen(3100)
